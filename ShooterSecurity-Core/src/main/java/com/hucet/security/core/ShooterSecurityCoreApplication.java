83ca4203a4280eee81eec26259d32e6a6b78d5d9
taesu <kimtaesoo188@gmail.com>
2017-01-19 12:27:40 +0900

    Initial commit

---
+++ ShooterSecurity-Core/src/main/java/com/hucet/security/core/ShooterSecurityCoreApplication.java
@@ -0,0 +1,12 @@
+package com.hucet.security.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterSecurityCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShooterSecurityCoreApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hucet/security/ShooterSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.hucet.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShooterSecurityApplication.class, args);
+	}
+}
