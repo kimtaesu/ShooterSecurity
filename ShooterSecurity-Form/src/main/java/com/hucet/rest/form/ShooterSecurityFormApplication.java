77f22a152b8d3f0d876e08538e9e8dfbd6d90664
taesu <kimtaesoo188@gmail.com>
2017-01-21 16:22:43 +0900

    add form module

---
+++ ShooterSecurity-Form/src/main/java/com/hucet/rest/form/ShooterSecurityFormApplication.java
@@ -0,0 +1,12 @@
+package com.hucet.rest.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterSecurityFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShooterSecurityFormApplication.class, args);
+    }
+}
